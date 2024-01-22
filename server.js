19d0f4fce912a0cf26ef6a8901edafa3920e30f7
Ritik <ritik11oct2003@gmail.com>
2024-01-22 19:42:08 +0530

    3 commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const cloudinary = require("cloudinary");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -14,7 +15,7 @@ app.use(express.json({ limit: "100mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
 app.use(fileUpload({ limits: { fileSize: 100 * 1024 * 1024 } }));
-
+app.use(cors());
 connectDb();
 
 cloudinary.config({
