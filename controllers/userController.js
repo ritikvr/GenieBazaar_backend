c2bb0c0515665f0c22dc00242b5451076987944b
Ritik <ritik11oct2003@gmail.com>
2024-01-22 20:43:53 +0530

    7 commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,7 +64,8 @@ exports.loginUser = async (req, res) => {
           expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
           ),
-          // httpOnly: true,
+          secure: true,
+          httpOnly: true,
         })
         .json({
           user,
